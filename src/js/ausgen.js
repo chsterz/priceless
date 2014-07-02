26466cb9714dd9b4d5dc783adcf3046193a4191a
cmichi <mich@elmueller.net>
2014-07-02 19:52:33 +0200

    improving generation of pdf

--- src/js/ausgen.js
+++ src/js/ausgen.js
@@ -15,13 +15,71 @@ var agencies = [
 	}
 ]
 
-var doc = new jsPDF();
+var doc = new jsPDF('p', 'mm', 'a4');
+var addr = {
+	  name: ''
+	, street: ''
+	, city: ''
+	, zipcode: ''
+}
 
 function generateLetter() {
-	doc.text(20, 20, 'Hello world!');
-	doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+	// letter layout according to http://upload.wikimedia.org/wikipedia/commons/6/64/DIN_5008%2C_Form_A.svg
+
+	// faltmarken 
+	doc.setLineWidth(0.5);
+	doc.line(0, 87, 10, 87);
+
+	doc.setLineWidth(0.5);
+	doc.line(0, 148.5, 10, 148.5);
+
+	//doc.setFont(['Times', 'Roman']).setFontSize(12);
+	//doc.text(20, 20, 'Hello world!');
+	//doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+
+	addr.name = $("#addr_name").val();
+	addr.street = $("#addr_street").val();
+	addr.zipcode = $("#addr_zipcode").val();
+	addr.city = $("#addr_city").val();
+	var send_back_to = [addr.name, addr.street, addr.zipcode, addr.city].join(', ');
+
+	doc.setFontSize(10);
+	doc.text(20, 27, send_back_to);
+	console.log(send_back_to);
+
+
 	doc.addPage();
 	doc.text(20, 20, 'Do you like that?');
+	if (photo)
+		doc.addImage(photo, 'JPEG', 15, 40, 180, 160);
+}
+var photo;
+
+function getPhoto(evt) {
+	console.log('getphoto');
+	var file = evt.target.files[0];
+	var reader = new FileReader();
+
+	//if (!file.type.match('image.')) {
+	//continue;
+	//}
+
+	var reader = new FileReader();
+
+	reader.onload = (function(theFile) {
+		return function(e) {
+			//console.log(e.target.result);
+			photo = e.target.result;
+
+			//updatePane();
+		};
+	})(file);
+
+	reader.readAsDataURL(file)
+
+	//console.log(data);
+	//console.log(file);
+	//console.log(evt.target);
 }
 
 function updatePane() {
@@ -48,5 +106,7 @@ $(function() {
 		';
 		$("#agencies").append(html);
 	}
+	//$('body').on('change', 'passport_photo', getPhoto);
+	document.getElementById('passport_photo').addEventListener('change', getPhoto, false);
 
 });
