ad06925d837ef39d6aa2df60f077e3b961ab7f24
cmichi <mich@elmueller.net>
2014-07-02 18:28:17 +0200

    starting with data structure

--- src/js/ausgen.js
+++ src/js/ausgen.js
@@ -1,8 +1,41 @@
-var doc = new jsPDF();
-doc.text(20, 20, 'Hello world!');
-doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
-doc.addPage();
-doc.text(20, 20, 'Do you like that?');
+var agencies = [
+	{
+		  title: "Bundesamt für Verfassungsschutz (BVerf)"
+		, street: "Strasse"
+		, city: "Stadt"
+		, zipcode: "PLZ"
+		, country: "Germany"
+	}
+	, {
+		  title: "Landesamt für Verfassungsschutz BaWü"
+		, street: "Strasse"
+		, city: "Stadt"
+		, zipcode: "PLZ"
+		, country: "Germany"
+	}
+]
 
-// Save the PDF
-doc.save('Test.pdf');
+function generateLetter() {
+	var doc = new jsPDF();
+	doc.text(20, 20, 'Hello world!');
+	doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+	doc.addPage();
+	doc.text(20, 20, 'Do you like that?');
+
+	// Save the PDF
+	doc.save('Test.pdf');
+}
+
+$(function() {
+	for (var a in agencies) {
+		var agency = agencies[a];
+		var html = '\
+			<div class="checkbox">\
+			<label>\
+				<input type="checkbox"> ' + agency.title + '</label>\
+			</div>\
+		';
+		$("#agencies").append(html);
+	}
+
+});
