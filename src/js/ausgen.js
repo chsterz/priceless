7e053cc68bd2cbd20b6ac3bc6bc6decd822a3357
cmichi <mich@elmueller.net>
2014-07-03 11:42:16 +0200

    break before sending date, more breaks after subject, improved title text in form, added stavor 1/2/sonderfall, added attachment note, added agency subtitle in receiver address

--- src/js/ausgen.js
+++ src/js/ausgen.js
@@ -33,6 +33,8 @@ var addr = {
 	, street: ''
 	, city: ''
 	, zipcode: ''
+	, birthdate: ''
+	, birthplace: ''
 }
 var crlf = "\r\n";
 var texts = [
@@ -61,11 +63,41 @@ Auftragsdatenverarbeitung)" + crlf +
 "   - die Herkunft der Daten und die empfangenden Stellen oder Kategorien \
 von empfangenden Stellen, an die Daten übermittelt werden oder \
 wurden." + crlf + crlf +
-"Mit diesem Brief übersende ich Ihnen eine Personalausweiskopie. " +
 "Bitte teilen Sie mir ggfs. auch mit, auf welcher Rechtsgrundlage Daten \
-über mich übermittelt wurden." + crlf + crlf +
-"Schliesslich bitte ich um eine Eingangsbestätigung dieses Schreibens. " + 
-"Bis zu Ihrer Antwort widerspreche ich der Löschung über mich gesammelter Daten." 
+über mich übermittelt wurden." + crlf +
+"Meiner Anfrage liegt ein besonderes Informationsinteresse unter \
+Wahrnehmung meines verfassungsrechtlich verbürgten Grundrechts auf \
+informationelle Selbstbestimmung zugrunde." + crlf + crlf +
+"Schliesslich bitte ich um eine Eingangsbestätigung dieses Schreibens."
++ crlf + 
+"Bis zu Ihrer Antwort widerspreche ich der Löschung über mich \
+gesammelter Daten." + crlf + crlf + 
+"Mit diesem Brief übersende ich Ihnen eine Personalausweiskopie. " 
++ crlf + crlf + crlf +
+"Mit freundlichen Grüßen" + crlf
+
+, "Sehr geehrte Damen und Herren," + crlf + crlf +
+"geben Sie mir bitte auf der Grundlage von $rechtsgrundlage$ schriftlich \
+Auskunft über die durch Ihre Behörde (auch im Weg der \
+Auftragsdatenverarbeitung)" + crlf + 
+"   - zu meiner Person gespeicherten Daten," + crlf + 
+"   - den Zweck und die Rechtsgrundlage der Speicherung," + crlf + 
+"   - die Herkunft der Daten und die empfangenden Stellen oder Kategorien \
+von empfangenden Stellen, an die Daten übermittelt werden oder \
+wurden." + crlf + crlf +
+"Bitte teilen Sie mir ggfs. auch mit, auf welcher Rechtsgrundlage Daten \
+über mich übermittelt wurden." + crlf +
+"Meiner Anfrage liegt ein besonderes Informationsinteresse unter \
+Wahrnehmung meines verfassungsrechtlich verbürgten Grundrechts auf \
+informationelle Selbstbestimmung zugrunde." + crlf + 
+"Ich bin wie Andrea Röpke journalistisch tätig und \
+fürchte daher, dass auch über mich rechtswidrig Daten gesammelt \
+wurden und werden." + crlf  + crlf +
+"Schliesslich bitte ich um eine Eingangsbestätigung dieses Schreibens."
++ crlf + 
+"Bis zu Ihrer Antwort widerspreche ich der Löschung über mich \
+gesammelter Daten." + crlf + crlf + 
+"Mit diesem Brief übersende ich Ihnen eine Personalausweiskopie. " 
 + crlf + crlf + crlf +
 "Mit freundlichen Grüßen" + crlf
 ];
@@ -118,28 +150,44 @@ console.log(cnt);
 	addr.street = $("#addr_street").val() || 'Musterstr. 1';
 	addr.zipcode = $("#addr_zipcode").val() || '12345';
 	addr.city = $("#addr_city").val() || 'Musterstadt';
+	addr.birthdate = $("#addr_birtdate").val() || '01.01.1970';
+	addr.birthplace = $("#addr_birthplace").val() || 'Musterstadt';
 	var send_back_to = [addr.name, addr.street, addr.zipcode + " " + addr.city].join(', ');
+	var lines_send_back_to = doc.splitTextToSize(send_back_to, 80)
+
+	doc.setFontSize(9);
+	doc.text(25, 27, lines_send_back_to);
+
+	//doc.text(25, 27, send_back_to);
+	//console.log(send_back_to);
 
 	doc.setFontSize(10);
-	doc.text(20, 27, send_back_to);
-	console.log(send_back_to);
-
-	doc.setFontSize(12);
 	var now = new Date();
-	var date = "Datum: " + now.getDate() + "." + now.getMonth() + "." + now.getFullYear();
-	var sender = [addr.name, addr.street, addr.zipcode + " " + addr.city, date].join(crlf);
-	doc.text(125, 32, sender);
+	var date = crlf + "Datum: " + now.getDate() + "." + now.getMonth() + "." + now.getFullYear();
+	var birthdate = "Geboren am " + addr.birthdate + ", in " + addr.birthplace;
+	var sender = [addr.name, addr.street, addr.zipcode + " " + addr.city, birthdate, date].join(crlf);
+	//doc.text(125, 32, sender);
+	var lines_sender = doc.splitTextToSize(sender, 75)
+	doc.text(125, 32, lines_sender);
 
 	var rcvr = 
 		  receiver.title + crlf 
+		+ receiver.subtitle + crlf 
 		+ receiver.street + crlf 
 		+ receiver.zipcode + " " + receiver.city + crlf
 		+ receiver.country;
-	doc.text(20, 44.7, rcvr);
+	//doc.text(25, 44.7, rcvr);
+	var lines_rcvr = doc.splitTextToSize(rcvr, 80)
+	doc.text(25, 44.7, lines_rcvr);
 
 	var body = texts[ receiver.text ].replace("$rechtsgrundlage$", receiver.law);
-	var txt = 'Betreff: Antrag auf Aktenauskunft' + crlf  + crlf + body + crlf
-			+ crlf + addr.name;
+
+	var attachment = "";
+	if (receiver.text === 1 || receiver.text === 2) 
+		attachment = crlf + crlf + crlf + "Anhang: Personalausweiskopie";
+
+	var txt = 'Betreff: Antrag auf Aktenauskunft' + crlf  + crlf + crlf + body + crlf
+			+ crlf + addr.name + attachment;
 	//doc.text(25, 95.46, txt);
 	var lines = doc.splitTextToSize(txt, 155)
 	doc.text(25, 95.46, lines)
